000e0f3856ef47bac6d44ed552c6996f7c7bf844
Erin Sheldon <erin.sheldon@gmail.com>
2020-12-14 09:00:02 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dsfit",
+    version="0.1.0",
+    packages=find_packages(),
+    author='Erin Sheldon',
+    author_email='erin.sheldon@gmail.com',
+    url='https://github.com/esheldon/dsfit',
+)
